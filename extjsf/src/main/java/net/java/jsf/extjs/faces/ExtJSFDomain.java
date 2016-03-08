2f10e12a80079419aa2c1456633f52fc55e7569e
Anton Baukin <anton.baukin@gmail.com>
2016-03-08 13:45:48 +0300

    0z.4 {temp, alpha, domain} Shortcut variable ${extDom}.

---
+++ extjsf/src/main/java/net/java/jsf/extjs/faces/ExtJSFDomain.java
@@ -0,0 +1,31 @@
+package net.java.jsf.extjs.faces;
+
+/* Spring Framework */
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/* extjsf: support */
+
+import static net.java.jsf.extjs.support.SpringPoint.bean;
+
+
+/**
+ * Class-variable. Shortcut for
+ * {@link RootView#getExtjsDomain()}.
+ *
+ * @author anton.baukin@gmail.com.
+ */
+@Component("extDom") @Scope("request")
+public class ExtJSFDomain
+{
+	/* Object */
+
+	public String toString()
+	{
+		return (domain != null)?(domain):
+		  (domain = bean(RootView.class).getExtjsDomain());
+	}
+
+	private String domain;
+}
